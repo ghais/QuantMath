2d4ca5ca45aaa78aa4dce978d5532c48ff240e02
Ghais <0x47@0x49.dev>
2022-12-23 21:33:09 -0500

    Add NaiveDate to Interpoable

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,34 @@
-extern crate statrs;
-extern crate ndarray;
+extern crate chrono;
 extern crate nalgebra;
+extern crate ndarray;
 extern crate rand;
 extern crate serde;
+extern crate statrs;
 //extern crate serde_state as serde;
 #[macro_use]
 extern crate serde_derive;
 //#[macro_use]
 //extern crate serde_derive_state;
 extern crate erased_serde;
-extern crate serde_tagged;
 extern crate serde_json;
+extern crate serde_tagged;
 #[macro_use]
 extern crate lazy_static;
-extern crate void;
-extern crate libc;
-extern crate finitediff;
 extern crate argmin;
-extern crate num_traits;
 extern crate derive_more;
+extern crate finitediff;
 extern crate levenberg_marquardt;
+extern crate libc;
+extern crate num_traits;
+extern crate void;
 // listed in dependency order, though this is not essential for compilation
 pub mod core;
-pub mod math;
-pub mod dates;
 pub mod data;
+pub mod dates;
+pub mod facade;
 pub mod instruments;
-pub mod risk;
+pub mod math;
 pub mod models;
 pub mod pricers;
+pub mod risk;
 pub mod solvers;
-pub mod facade;
